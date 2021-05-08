abdf13bacfce4ca716a7aafeb16f8f0e3f527648
sgunpreetsandhu01 <62554005+sgunpreetsandhu01@users.noreply.github.com>
2021-05-08 08:36:30 +0530

    Add files via upload

---
+++ committee/committee.js
@@ -0,0 +1,6 @@
+document.getElementById("b1").addEventListener("click",myFunction);
+function myFunction()
+{
+    document.querySelector(".pop").innerHTML="<p>Dr. Chitra A. Associate professor Department of Energy & Power Electronics School of Electrical Engineering VIT Vellore, India E-Mail: chitra.a@vit.ac.in Mobile:+91 9894760447 </p>";
+}
+
