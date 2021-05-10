d2f3c354a69d1483285ccce8904ba472511b45ca
Hritam Dutta <hritamloyola10@gmail.com>
2021-05-11 03:45:02 +0530

    Committee Page Final

--- committee/committee.js
+++ committee/committee.js
@@ -1,21 +1,35 @@
 document.getElementById("b1").addEventListener("click",myFunction);
 function myFunction()
 {
-    document.getElementById("pop").className="main4";
-    document.querySelector("#pop").innerHTML="<p><b>Dr. Chitra A.</b><br> Associate professor<br>Department of Energy & Power Electronics School of Electrical Engineering<br>VIT Vellore, India<br> E-Mail: chitra.a@vit.ac.in <br>Mobile:+91 9894760447 </p>";
-    //document.getElementById("b1").style.backgroundColor="#141A2E"
+    document.getElementById("pop").className="main4 shadow";
+    document.querySelector("#pop").innerHTML="<p><b>Dr. Chitra A.</b><br> Associate professor<br>Department of Energy & Power Electronics School of Electrical Engineering<br>VIT Vellore, India<br> E-Mail: chitra.a@vit.ac.in <br>Mobile:+91 9894760447 </p><br>";
 }
 document.getElementById("b2").addEventListener("click",myFunction1);
 function myFunction1()
 {
-    document.getElementById("pop").className="main4";
-    document.querySelector("#pop").innerHTML="<p><b>Dr. V. Indragandhi </b><br> Associate professor<br>Department of Energy & Power Electronics School of Electrical Engineering<br>VIT Vellore, India<br> E-Mail: indragandhi.v@vit.ac.in <br>Mobile:+91 97506 03539 </p>";
-    //document.getElementById("b2").style.backgroundColor="#141A2E"
+    document.getElementById("pop").className="main4 shadow";
+    document.querySelector("#pop").innerHTML="<p><b>Dr. V. Indragandhi </b><br> Associate professor<br>Department of Energy & Power Electronics School of Electrical Engineering<br>VIT Vellore, India<br> E-Mail: indragandhi.v@vit.ac.in <br>Mobile:+91 97506 03539 </p><br>";
 }
 document.getElementById("b3").addEventListener("click",myFunction2);
 function myFunction2()
 {
-    document.getElementById("pop").className="main4";
-    document.querySelector("#pop").innerHTML="<p><b>Dr. Razia Sultana W.</b><br> Associate professor<br>Department of Energy & Power Electronics School of Electrical Engineering<br>VIT Vellore, India<br> E-Mail: wraziasultana@vit.ac.in <br>Mobile:+91 9943295041</p>";
-    //document.getElementById("b3").style.backgroundColor="#141A2E"
+    document.getElementById("pop").className="main4 shadow";
+    document.querySelector("#pop").innerHTML="<p><b>Dr. Razia Sultana W.</b><br> Associate professor<br>Department of Energy & Power Electronics School of Electrical Engineering<br>VIT Vellore, India<br> E-Mail: wraziasultana@vit.ac.in <br>Mobile:+91 9943295041</p><br>";
 }
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+  
